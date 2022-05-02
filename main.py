9dabe8375e0d76eb22f1099e911d805135bb7380
Jedaurran <100149369+Jedaurran@users.noreply.github.com>
2022-05-02 09:02:16 -0500

    Actualizar pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ while True:
 
     val = pins.analog_read_pin(AnalogPin.P0)
     basic.show_number(val)
+    serial.write_value("val", val)
--- main.ts
+++ main.ts
@@ -2,4 +2,5 @@ let val: number;
 while (true) {
     val = pins.analogReadPin(AnalogPin.P0)
     basic.showNumber(val)
+    serial.writeValue("val", val)
 }
